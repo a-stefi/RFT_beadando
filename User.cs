42fd78a394a728c106f2b95848af7b15db763895
tothandras1011-boop <tothandras1011@gmail.com>
2026-01-20 22:32:38 +0100

    Add files via upload

---
+++ User.cs
@@ -0,0 +1,21 @@
+﻿public class User
+
+{
+
+    public required string Username { get; set; }
+
+    public required string AccountNumber { get; set; }
+
+    public required string Pin { get; set; }
+
+    public required string PhoneNumber { get; set; }
+
+    public string BirthPlace { get; set; }
+
+    public DateTime BirthDate { get; set; }
+
+    public int Balance { get; set; } = 100000;
+
+    public List<string> Transactions { get; set; } = new List<string>();
+
+}
