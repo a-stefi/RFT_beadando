7047cb62d8c47a19e7ca2aa603756ca08337c7d1
tothandras1011-boop <tothandras1011@gmail.com>
2026-01-20 22:36:48 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+
+namespace ATMApp
+{
+    internal class Program
+    {
+        namespace ATMApp
+
+    {
+
+        public class Program
+
+        {
+
+            [STAThread]
+
+            public static void Main()
+
+            {
+
+                Application app = new Application();
+
+                SplashScreenWindow splashScreen = new SplashScreenWindow();
+
+                app.Run(splashScreen);
+
+            }
+
+        }
+
+    }
+}
+}
---
+++ UserRepository.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using System.IO;
+using System.Text.Json;
+
+namespace ATMApp
+{
+    internal class UserRepository
+    {
+        public class userRepository
+
+        {
+
+            private const string FilePath = "users.json";
+
+
+
+            public List<User> LoadUsers()
+
+            {
+
+                if (!File.Exists(FilePath))
+
+                    return new List<User>();
+
+                var json = File.ReadAllText(FilePath);
+
+                return JsonSerializer.Deserialize<List<User>>(json) ?? new List<User>();
+
+            }
+
+
+
+            public void SaveUsers(List<User> users)
+
+            {
+
+                var json = JsonSerializer.Serialize(users, new JsonSerializerOptions { WriteIndented = true });
+
+                File.WriteAllText(FilePath, json);
+
+            }
+
+
+
+            public void SaveUser(User user)
+
+            {
+
+                var users = LoadUsers();
+
+                var index = users.FindIndex(u => u.AccountNumber == user.AccountNumber);
+
+                if (index != -1)
+
+                {
+
+                    users[index] = user;
+
+                    SaveUsers(users);
+
+                }
+
+            }
+
+        }
+    }
+}
