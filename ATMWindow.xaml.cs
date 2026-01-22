103e068ac82100b50ec376bc08e07b7f5e62bdd8
tothandras1011-boop <tothandras1011@gmail.com>
2026-01-22 19:19:15 +0100

    Gyorspénz gombok eseménykezelése

--- ATMWindow.xaml.cs
+++ ATMWindow.xaml.cs
@@ -36,6 +36,14 @@ namespace ATMApp
         {
             MessageBox.Show($"Jelenlegi egyenleg: {_currentUser.Balance} Ft", "Egyenleg", MessageBoxButton.OK, MessageBoxImage.Information);
         }
+
+        private void FastCash_Click(object sender, RoutedEventArgs e)
+        {
+            if (sender is Button button && int.TryParse(button.Tag.ToString(), out int amount))
+            {
+                PénzFelvételVégrehajtása(amount);
+            }
+        }
         
         private void WithdrawButton_Click(object sender, RoutedEventArgs e)
         {
